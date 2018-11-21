362e6c1fb8fb50b748e568daf15562fb2d020c37
YouFn <1422135791@qq.com>
2018-11-21 17:04:56 +0800

    美购网系统代码文件夹

---
+++ core/src/main/java/com/felix/grouppurchase/GroupPurchaseApplication.java
@@ -0,0 +1,12 @@
+package com.felix.grouppurchase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupPurchaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupPurchaseApplication.class, args);
+    }
+}
---
+++ core/src/test/java/com/felix/grouppurchase/GroupPurchaseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.felix.grouppurchase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GroupPurchaseApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
