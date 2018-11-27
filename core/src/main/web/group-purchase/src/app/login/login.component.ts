3cec6834a87aa7a2aa7c5dd5fe2026cebb6dc89f
huangcw111 <519873176@qq.com>
2018-11-27 22:38:57 +0800

    登录代码完善

--- core/src/main/web/group-purchase/src/app/login/login.component.ts
+++ core/src/main/web/group-purchase/src/app/login/login.component.ts
@@ -7,14 +7,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   loginWay:any='mobile';  //登陆方式
-  id:any;   //用户名
-  password:any;//密码
+  id:any='';   //用户名
+  password:any='';//密码
+  code:any=''; //验证码
+  username:any="";
   constructor() { }
 
   ngOnInit() {
+    this.username="";
+
   }
+
   //改变登陆方式
   changeLoginWay(way){
     this.loginWay=way;
   }
+
+  //提交登录
+  submitData(){
+    if(this.loginWay=='Id'){
+      if(this.id==''){
+        alert("请输入账号");
+        return;
+      }
+      else{
+        if(this.password==''){
+          alert("请输入密码");
+          return;
+        }
+        else{
+          let url="http://localhost:8080/gpsys/user/login";
+          let send={
+            id:this.id,
+            password:this.password
+          };
+          $.ajax(url,{
+            data:send,
+            dataType:"jsonp",
+            jsonp:"callback",
+            type:"GET",
+            success:json=>{
+              if(json.stage=='1'){
+                this.username=json.data.userName;
+              }
+              else{
+                alert("登录失败："+json.msg);
+              }
+            }
+          }
+      )
+
+    }
+
+  }
+
+}
+}
 }
