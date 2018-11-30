66bc082ef22db76ba7d03ceb530df4ae94ff514b
huangcw111 <519873176@qq.com>
2018-11-30 14:38:24 +0800

    我的返利

---
+++ core/src/main/web/group-purchase/src/app/login/my-rebate/my-rebate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-rebate',
+  templateUrl: './my-rebate.component.html',
+  styleUrls: ['./my-rebate.component.css']
+})
+export class MyRebateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
