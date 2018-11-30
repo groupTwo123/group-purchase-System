dcdd73706af680f2488d397c6e3a93928b86859a
huangcw111 <519873176@qq.com>
2018-11-30 15:22:33 +0800

    商家注册

---
+++ core/src/main/web/group-purchase/src/app/business-login/business-register/business-register.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-business-register',
+  templateUrl: './business-register.component.html',
+  styleUrls: ['./business-register.component.css']
+})
+export class BusinessRegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
