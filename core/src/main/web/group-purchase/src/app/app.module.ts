b1a0141867f3618e047820be246e365a4ce7f256
huangcw111 <519873176@qq.com>
2018-11-30 15:15:29 +0800

    修改路径

--- core/src/main/web/group-purchase/src/app/app.module.ts
+++ core/src/main/web/group-purchase/src/app/app.module.ts
@@ -5,15 +5,17 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+import { RegisterComponent } from './login/register/register.component';
 import { HomePageComponent } from './login/home-page/home-page.component';
 import{RouterModule,Routes} from "@angular/router";
 import { ShoppingCardPageComponent } from './login/shopping-card-page/shopping-card-page.component';
 import { MyOrdersComponent } from './login/my-orders/my-orders.component';
 import { MyRebateComponent } from './login/my-rebate/my-rebate.component';
 import { PersonalCenterComponent } from './login/personal-center/personal-center.component';
-import { BusinessHomepageComponent } from './login/business-homepage/business-homepage.component';
-import { AddCommodityComponent } from './login/business-homepage/add-commodity/add-commodity.component';
+import { BusinessHomepageComponent } from './business-login/business-homepage/business-homepage.component';
+import { AddCommodityComponent } from './business-login/business-homepage/add-commodity/add-commodity.component';
+import { BusinessLoginComponent } from './business-login/business-login.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
 
 const appRoutes:Routes=[
 	{path:'',redirectTo:'login',pathMatch:'full'},
@@ -41,7 +43,9 @@ const appRoutes:Routes=[
     MyRebateComponent,
     PersonalCenterComponent,
     BusinessHomepageComponent,
-    AddCommodityComponent
+    AddCommodityComponent,
+    BusinessLoginComponent,
+    AdminLoginComponent
   ],
   imports: [
     BrowserModule,
