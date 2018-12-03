87d345736fe6a3b52a8338b935e16fde39a659a2
DavidYouFN <1422135791@qq.com>
2018-12-03 14:52:28 +0800

    commit second

--- core/src/main/java/com/felix/grouppurchase/GroupPurchaseApplication.java
+++ core/src/main/java/com/felix/grouppurchase/GroupPurchaseApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GroupPurchaseApplication {
 
     public static void main(String[] args) {
+        System.out.println("");
         SpringApplication.run(GroupPurchaseApplication.class, args);
     }
 }
