76c478d15907219a8b433bee29d8a7cb99b7533f
huangcw111 <519873176@qq.com>
2018-12-04 08:24:33 +0800

    商家页面

--- core/src/main/web/group-purchase/src/app/business-login/business-login.component.ts
+++ core/src/main/web/group-purchase/src/app/business-login/business-login.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./business-login.component.css']
 })
 export class BusinessLoginComponent implements OnInit {
-
+  loginWay:any='0';// 0为账号密码登录 1为手机登录
   constructor() { }
 
   ngOnInit() {
-  }
+    document.getElementById("loginBox").style.height=(document.body.scrollHeight).toString()+'px';
+    document.getElementById("loginBox").style.width=(document.body.scrollWidth).toString()+'px';
 
+  }
+  //切换登录方式
+  changeLoginWay(way){
+    this.loginWay=way;
+  }
 }
--- core/src/main/web/group-purchase/src/app/business-login/business-register/business-register.component.ts
+++ core/src/main/web/group-purchase/src/app/business-login/business-register/business-register.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./business-register.component.css']
 })
 export class BusinessRegisterComponent implements OnInit {
-
+  step:number=0;
   constructor() { }
 
   ngOnInit() {
+    this.step=0;
+    document.getElementById("registerBox").style.height=(document.body.scrollHeight).toString()+'px';
+    document.getElementById("registerBox").style.width=(document.body.scrollWidth).toString()+'px';
   }
-
+  //下一步点击炒作
+  nextStyleFun(){
+    this.step++;
+  }
 }
