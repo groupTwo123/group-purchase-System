7e57da6d0b5e533e7c53477a5576b2c17ed09219
DavidYouFN <1422135791@qq.com>
2018-12-06 08:23:01 +0800

    上传文件、图片、视频等配置文件

---
+++ core/src/main/java/com/felix/grouppurchase/config/WebAppConfig.java
@@ -0,0 +1,63 @@
+package com.felix.grouppurchase.config;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+import javax.servlet.MultipartConfigElement;
+
+/**
+ * @Author fangyong
+ * @Description
+ * @Date 2018/12/5 9:50
+ * @Param
+ * @return
+ **/
+
+//上传配置类
+//图片放到/E:/fileUpload/后，从磁盘读取的图片数据scr将会变成images/picturename.jpg的格式
+@Configuration
+public class WebAppConfig extends WebMvcConfigurerAdapter {
+//public class WebAppConfig extends WebMvcConfigurationSupport {
+    /**
+     * 在配置文件中配置的文件保存路径
+     */
+    @Value("${cbs.imagesPath}")
+    private String mImagesPath;
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement(){
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //文件最大KB,MB
+        factory.setMaxFileSize("1024MB");
+        //设置总上传数据总大小
+        factory.setMaxRequestSize("1024MB");
+        return factory.createMultipartConfig();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        if(mImagesPath.equals("") || mImagesPath.equals("${cbs.imagesPath}")){
+            String imagesPath = WebAppConfig.class.getClassLoader().getResource("").getPath();
+            System.out.print("1.上传配置类imagesPath=="+imagesPath+"\n");
+            if(imagesPath.indexOf(".jar")>0){
+                imagesPath = imagesPath.substring(0, imagesPath.indexOf(".jar"));
+            }else if(imagesPath.indexOf("classes")>0){
+                imagesPath = "file:"+imagesPath.substring(0, imagesPath.indexOf("classes"));
+            }
+            imagesPath = imagesPath.substring(0, imagesPath.lastIndexOf("/"))+"/images/";
+            mImagesPath = imagesPath;
+        }
+        System.out.print("imagesPath============="+mImagesPath+"\n");
+        //LoggerFactory.getLogger(WebAppConfig.class).info("imagesPath============="+mImagesPath+"\n");
+        registry.addResourceHandler("/images/**").addResourceLocations(mImagesPath);
+        // TODO Auto-generated method stub
+        System.out.print("2.上传配置类mImagesPath=="+mImagesPath+"\n");
+        super.addResourceHandlers(registry);
+    }
+
+}
+
