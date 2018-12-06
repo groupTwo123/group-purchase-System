6dfde51df684172f2bfe75569626e11463afd6d6
DavidYouFN <1422135791@qq.com>
2018-12-06 08:25:21 +0800

    上传图片以及查询图片接口

--- core/src/main/java/com/felix/grouppurchase/controller/CommodityController.java
+++ core/src/main/java/com/felix/grouppurchase/controller/CommodityController.java
@@ -1,17 +1,27 @@
 package com.felix.grouppurchase.controller;
 
+import com.felix.grouppurchase.model.CommodityPicture;
 import com.felix.grouppurchase.service.ICommodityService;
+import com.felix.grouppurchase.util.JsonTransfer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @Date: 2018/11/27 22:46
  * @Author: huangchuwen
  */
 @RestController
-@RequestMapping("/gpsys/commodity")
+@RequestMapping("/c/commodity")
 public class CommodityController {
 
     @Autowired
@@ -96,6 +106,65 @@ public class CommodityController {
     public String getCommodityByName(String commodityName, String callback){
        return commodityService.getCommodityByName(commodityName, callback);
     }
-    
-    
+
+    /**
+     * @Author fangyong
+     * @Description 上传商品图片
+     * @Date 2018/12/5 8:54
+     * @Param
+     * @return
+     **/
+    @RequestMapping(value = "/addCommodityPicture", method = RequestMethod.GET)
+    public String addCommodityPicture(@RequestParam("file") MultipartFile file, String callback) throws IOException {
+        String url;
+        System.out.print("上传文件===" + "\n");
+        //判断文件是否为空
+        if (file.isEmpty()) {
+            return "上传文件不可为空";
+        }
+        // 获取文件名
+        String fileName = file.getOriginalFilename();
+        fileName = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()) + "_" + fileName;
+        //加个时间戳，尽量避免文件名称重复
+        String path = "E:/fileUpload/" + fileName;
+        //创建文件路径
+        File dest = new File(path);
+        //判断文件是否已经存在
+        if (dest.exists()) {
+            return "文件已经存在";
+        }
+
+        //判断文件父目录是否存在
+        if (!dest.getParentFile().exists()) {
+            dest.getParentFile().mkdir();
+        }
+
+        JsonTransfer s = new JsonTransfer();
+        try {
+            //上传文件
+            file.transferTo(dest); //保存文件
+            url = "http://localhost:8080/gpsys/commodity/addCommodityPicture/" + fileName;//本地运行项目
+            int result = commodityService.addCommodityPicture(fileName, path, url, callback);
+            String result1 = s.result(1,"上传成功",path,callback);
+            return result1;
+
+        } catch (IOException e) {
+            String result1 = s.result(0,"上传失败","",callback);
+            return result1;
+        }
+    }
+
+    /**
+     * @Author fangyong
+     * @Description 获取商品图片
+     * @Date 2018/12/5 10:57
+     * @Param
+     * @return
+     **/
+    @RequestMapping(value = "/getCommodityPicture", method = RequestMethod.GET)
+    public List<CommodityPicture> getCommodityPicture(String callback){
+        List<CommodityPicture> pictureList = commodityService.getCommodityPicture(callback);
+        return pictureList;
+    }
+
 }
