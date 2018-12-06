bcb82199238a893f9af75a89a2611e918e8a9db8
huangcw111 <519873176@qq.com>
2018-12-06 17:30:34 +0800

    加载动画

---
+++ core/src/main/web/group-purchase/src/app/search-loading/search-loading.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-loading',
+  templateUrl: './search-loading.component.html',
+  styleUrls: ['./search-loading.component.css']
+})
+export class SearchLoadingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
