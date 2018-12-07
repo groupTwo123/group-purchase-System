e982d3fe9aaa1e4d4afe2185f68918cd05f5bef0
huangcw111 <519873176@qq.com>
2018-12-07 17:52:32 +0800

    增加主键id为自增

--- sql/shoppingCar.sql
+++ sql/shoppingCar.sql
@@ -1,7 +1,9 @@
+use group_purchase;
 CREATE TABLE `tb_shopping_car` (
+  `id`  int(11) NOT NULL AUTO_INCREMENT COMMENT '自增id',
   `commodity_id` varchar(50) NOT NULL COMMENT '商品id',
   `commodity_number` bigint(20) DEFAULT NULL COMMENT '购物车中商品的数量',
   `volume_id` varchar(32) DEFAULT NULL COMMENT '仓库id',
   `user_id` varchar(32) DEFAULT NULL COMMENT '用户id',
-  PRIMARY KEY (`commodity_id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
