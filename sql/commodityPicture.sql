44e08f5c2cffdc940601dcdb07b3f90137660714
huangcw111 <519873176@qq.com>
2018-12-12 08:33:09 +0800

    字段修改

--- sql/commodityPicture.sql
+++ sql/commodityPicture.sql
@@ -1,8 +1,9 @@
+use group_purchase;
 CREATE TABLE `tb_commodity_picture` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增id',
   `commodity_id` varchar(32) DEFAULT NULL COMMENT '商品id',
-  `url` varchar(100) DEFAULT NULL COMMENT '图片地址',
-  `local_url` varchar(100) DEFAULT NULL COMMENT '图片所存的本地地址',
+  `url` LONGTEXT DEFAULT NULL COMMENT '图片地址',
   `priority` int(4) DEFAULT NULL COMMENT '图片优先级存储',
+  
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
