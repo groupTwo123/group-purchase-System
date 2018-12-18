769e90b738b4df68c032ebf03db69ee3c54f6f82
huangcw111 <519873176@qq.com>
2018-12-18 14:47:53 +0800

    图片表字段修正

--- sql/commodityPicture.sql
+++ sql/commodityPicture.sql
@@ -1,9 +1,9 @@
 use group_purchase;
 CREATE TABLE `tb_commodity_picture` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增id',
-  `commodity_id` varchar(32) DEFAULT NULL COMMENT '商品id',
-  `url` LONGTEXT DEFAULT NULL COMMENT '图片地址',
+  `picId` varchar(32) DEFAULT NULL COMMENT '图片id',
+  `picBase64` longtext COMMENT '图片base64编码',
+  `picType` tinyint(2) DEFAULT NULL COMMENT '图片类型  1:用户头像  2:商品图片 3:商品描述图片',
   `priority` int(4) DEFAULT NULL COMMENT '图片优先级存储',
-  
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
